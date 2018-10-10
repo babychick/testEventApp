e7a51db4340736d58050802e633fa74d6479c82e
longtran123 <longb1400767@gmail.com>
2018-10-10 23:15:00 +0700

    Data vị trí

---
+++ ME/src/data/locations.js
@@ -0,0 +1,38 @@
+var locations = [
+    {
+    latitude: 10.031114,
+    longitude: 105.771645,
+    title:'Đại học Cần Thơ',
+    description: 'Ngập lụt'
+    },
+    {
+    latitude: 10.033365, 
+    longitude: 105.780541,
+    title:'CV Lưu Hữu Phước',
+    description: 'Ngập lụt'
+    }
+    ,
+    {
+    latitude: 10.031202, 
+    longitude: 105.775071, 
+    title:'Củi Coffee & Homestay',
+    description: 'Số 1a, hẻm 95, Đường Mậu Thân'
+    }
+    ,
+    {
+    latitude: 10.029267, 
+    longitude: 105.769559,
+    title:'Hội trường Rùa',
+    description: 'Đại học Cần Thơ'
+    }
+    ,
+    {
+    latitude: 10.026686, 
+    longitude: 105.771219, 
+    title:'Cửa Hàng Máy Văn Phòng Quang',
+    description: '146G, Đường Trần Văn Hoài, Quận Ninh Kiều, Xuân Khánh, Ninh Kiều, Cần Thơ, Việt Nam'
+    }
+];
+
+
+module.exports = locations;
