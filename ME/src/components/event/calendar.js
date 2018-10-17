f6caafda38cccd6049df65566a6563189c8cf0aa
babychick <philx9677@gmail.com>
2018-10-17 07:43:06 +0700

    calendar
    Hiện danh sách sự kiện khi click vào ngày

--- ME/src/components/event/calendar.js
+++ ME/src/components/event/calendar.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { Color } from '../../assets/color';
+import moment from 'moment';
+import url from '../../assets/url';
+import { Item } from '../common/item';
+import { CalendarItem } from '../common/calendarItem';
+
+var startDate = moment().format('DD-MM-YYYY');
 
 class CalendarScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            eventList: [],
+            backColorItem: null
         }
     }
 
     componentDidMount() {
+        fetch( url + 'event/findByDate/' + startDate + 'T17:00:00.000Z')
+            .then(data => data.json())
+            .then(dataJson => {
+                this.setState({
+                    eventList: dataJson
+                })
+            })
+            .catch(err => {
+                // alert('Không tìm thấy sự kiện!')
+            })
+    }
 
+
+   fetchByDate = async (date) => {
+        await fetch( url + 'event/findByDate/' + date.dateString + 'T17:00:00.000Z')
+            .then(data => data.json())
+            .then(dataJson => {
+                this.setState({
+                    eventList: dataJson
+                })
+                console.log(this.state.eventList);
+            })
+            .catch(err => {
+                alert('Không tìm thấy sự kiện!')
+            })
+        console.log(date.dateString);
+        console.log(url + 'event/findByDate/' + date.dateString + 'T17:00:00.000Z');
+        console.log(this.state.eventList);
     }
 
     render() {
@@ -27,12 +62,24 @@ class CalendarScreen extends React.Component {
                     textDayFontSize: 16,
                     textMonthFontSize: 16,
                     textDayHeaderFontSize: 16,
-                    textMonthFontWeight: 'bold'
-                    }}/>
+                    textMonthFontWeight: 'bold',
+                    }}
+                    onDayPress={this.fetchByDate}/>
 
-                <View style={styles.searchDate}>
+                <Text style={styles.subTitle}>Sự kiện trong ngày</Text>
 
-                </View>
+                <ScrollView style={styles.list}>
+                    {
+                        this.state.eventList.map((item, key) => {
+                            if (key % 2 === 0) {
+                            }
+                            <CalendarItem start={item.startTime} end={item.endTime} eventName={item.eventName} backgroundColor={Color._50} key={key}/>
+                        }
+
+                            
+                        )
+                    }
+                </ScrollView>
 
             </View>
         )
@@ -45,8 +92,16 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         backgroundColor: '#fff'
     },
-    searchDate: {
-
+    list: {
+        marginTop: 8
+    },
+    subTitle: {
+        fontSize: 14,
+        backgroundColor: '#EEEEEE',
+        paddingVertical: 8,
+        paddingLeft: 16,
+        marginTop: 24,
+        textAlign: 'center'
     }
 })
 
